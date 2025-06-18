015f848b6fd430ae0400f9db98e6dbdcd407fc05
PhD candidate <63307716+Schouwenaar@users.noreply.github.com>
2025-06-18 14:52:17 +0200

    per jaar per buurt 1 rij

--- Code voor mergen.R
+++ Code voor mergen.R
@@ -1,9 +1,16 @@
 library(dplyr)
 library(readr)
-merged_data <- nabijheid_voorzieningen2016 %>%
-  full_join(nabijheid_voorzieningen2020, by = "Regioaanduiding.Codering..code.", suffix = c("_2016", "_2020")) %>%
-  full_join(nabijheid_voorzieningen2022, by = "Regioaanduiding.Codering..code.")
+nabijheid_voorzieningen2016 <- read_csv("~/Documents/School_Opdracht/data/Data/nabijheid_voorzieningen2016.csv")
+nabijheid_voorzieningen2020 <- read_csv("~/Documents/School_Opdracht/data/Data/nabijheid_voorzieningen2020.csv")
+nabijheid_voorzieningen2022 <- read_csv("~/Documents/School_Opdracht/data/Data/nabijheid_voorzieningen2022.csv")
+AlcoholBuurten <- read_csv("data/AlcoholBuurten.csv")
+nabijheid_voorzieningen2016$Perioden <- 2016
+nabijheid_voorzieningen2020$Perioden <- 2020
+nabijheid_voorzieningen2022$Perioden <- 2022
+
 nabijheid_voorzieningen2016$...1 <- NULL
+
+
 voorzien <- rbind(nabijheid_voorzieningen2016, nabijheid_voorzieningen2020,nabijheid_voorzieningen2022)
 write_csv(voorzien,"data/Data/voorzieningen.csv")
 merged_data <- voorzien %>%
