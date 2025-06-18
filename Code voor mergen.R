a3596ebcd6155fa6d704f5f4aef859045c00d58e
Tombo14 <tomhaandrikman@gmail.com>
2025-06-18 14:35:42 +0200

    Mergen

---
+++ Code voor mergen.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(readr)
+merged_data <- nabijheid_voorzieningen2016 %>%
+  full_join(nabijheid_voorzieningen2020, by = "Regioaanduiding.Codering..code.", suffix = c("_2016", "_2020")) %>%
+  full_join(nabijheid_voorzieningen2022, by = "Regioaanduiding.Codering..code.")
+nabijheid_voorzieningen2016$...1 <- NULL
+voorzien <- rbind(nabijheid_voorzieningen2016, nabijheid_voorzieningen2020,nabijheid_voorzieningen2022)
+write_csv(voorzien,"data/Data/voorzieningen.csv")
+merged_data <- voorzien %>%
+  merge(AlcoholBuurten, by = "Regioaanduiding.Codering..code.")
