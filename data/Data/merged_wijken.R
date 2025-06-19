ad4313cd3c423b29efca3d9f8efbda4ed790d41e
DerekVDL <d.r.c.vander.linden@student.vu.nl>
2025-06-19 11:16:35 +0200

    Update merged_wijken.R

--- data/Data/merged_wijken.R
+++ data/Data/merged_wijken.R
@@ -1,3 +1,27 @@
+
+maps <- cbs_get_maps()
+prov_map_yr <- max(maps$year[maps$region == "provincie"])
+
+prov_sf <- cbs_get_sf(region = "provincie", year = prov_map_yr)
+
+zh_sf <- prov_sf %>% filter(statnaam == "Zuid-Holland")
+
+ggplot(zh_sf) +
+  geom_sf(fill = "#2b8cbe", colour = "white") +
+  theme_void() +
+  labs(title = paste0("Provincie Zuid-Holland – CBS kaart ", prov_map_yr))
+
+muni_sf <- cbs_get_sf("provincie", prov_map_yr)
+
+#wijken en buurten er over heen
+prov_map_yr <- max(cbs_get_maps()$year[cbs_get_maps()$region == "provincie"])
+zh_sf <- cbs_get_sf("provincie", prov_map_yr) %>%
+  filter(statnaam == "Zuid-Holland")
+
+wijk_map_yr <- max(cbs_get_maps()$year[cbs_get_maps()$region == "wijk"])
+wijken_sf <- cbs_get_sf("wijk", wijk_map_yr)
+
+
 merged_data <- merged_data %>%
   mutate(Distance_cafes = if_else(
     Horeca.Cafés.en.dergelijke.Afstand.tot.café.e.d...km.> 1,
@@ -5,52 +29,20 @@ merged_data <- merged_data %>%
     "Cafes closer than 1 km"
   ))
 
-write.csv(merged_data, "data/Data/merged_data.csv")
+#write.csv(merged_data, "data/Data/merged_data.csv")
 
 #multipolygon koppelen aan merged_goeie (die wijkcodes)
-library(cbsodataR)
-library(ggplot2)
-library(dplyr)
-library(sf)
-library(stringr)
-
-maps      <- cbs_get_maps()
-prov_year <- max(maps$year[maps$region == "provincie"])
-wijk_year <- max(maps$year[maps$region == "wijk"])
-
-zh_sf_raw     <- cbs_get_sf("provincie", prov_year) |>
-  filter(statnaam == "Zuid-Holland")
-
-wijken_sf_raw <- cbs_get_sf("wijk", wijk_year)
-
-gem_codes <- c("0599", "1931")                      # GM-codes → digits
-wijken_subset_raw <- wijken_sf_raw |>
-  mutate(gem_code = str_extract(statcode, "\\d{4}")) |>
-  filter(gem_code %in% gem_codes)
-
-zh_sf        <- st_as_sf(zh_sf_raw)
-wijken_subset <- st_as_sf(wijken_subset_raw)
-
-merged_2016 <- st_as_sf(merged_2016)
-
-
-
+wijken_sf<-sf::st_as_sf(wijken_sf)
 merged_2016 <- merged_data %>%
   filter(Perioden == 2016)
 
+merged_2016<- as.data.frame(merged_2016)
 
-merged_wijken <- wijken_sf_raw %>%
+#merged_2016$geometry <- NULL
+
+wijken_sf <- wijken_sf %>%
   rename("Regioaanduiding.Codering..code." = "statcode")
 
-merged_2016 <- merged_2016 %>% full_join(merged_wijken, by = "Regioaanduiding.Codering..code.")
-
-
-#filter alleen wijken
-
-wijken_data <- merged_2016 %>%
-  filter(Regioaanduiding.Soort.regio..omschrijving..x == "Wijk")
-
-
-
-
+merged_2016 <- merged_2016 %>% merge(wijken_sf, 
+                                         by = "Regioaanduiding.Codering..code.")
 
